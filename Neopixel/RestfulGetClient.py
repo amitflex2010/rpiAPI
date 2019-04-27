39d89c4945cb2199705da286d7dabb4da40b15db
Tripathi, Amit (ITCDEC) - KLM <Amit.Tripathi@klm.com>
2019-04-27 23:54:55 +0200

    added webservice

---
+++ Neopixel/RestfulGetClient.py
@@ -0,0 +1,25 @@
+#Python 2.7.6
+#RestfulClient.py
+
+import requests
+from requests.auth import HTTPDigestAuth
+import json
+
+# Replace with the correct URL
+url = "https://us-central1-mysampleproject-3b9ff.cloudfunctions.net/nodeapp/getContacts"
+
+# It is a good practice not to hardcode the credentials. So ask the user to enter credentials at runtime
+myResponse = requests.get(url)
+#print (myResponse.status_code)
+
+# For successful API call, response code will be 200 (OK)
+if(myResponse.ok):
+
+	# Loading the response data into a dict variable
+	# json.loads takes in only binary or string variables so using content to fetch binary content
+	# Loads (Load String) takes a Json file and converts into python data structure (dict or list, depending on JSON)
+    print("loaded")
+	
+else:
+  # If response code is not ok (200), print the resulting http error code with description
+	myResponse.raise_for_status()
