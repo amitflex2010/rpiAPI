deb431ed6a7e6f5647aaad3505fa9c248d48a585
Tripathi, Amit (ITCDEC) - KLM <Amit.Tripathi@klm.com>
2019-04-28 00:01:04 +0200

    parsed json

--- Neopixel/RestfulGetClient.py
+++ Neopixel/RestfulGetClient.py
@@ -18,7 +18,12 @@ if(myResponse.ok):
 	# Loading the response data into a dict variable
 	# json.loads takes in only binary or string variables so using content to fetch binary content
 	# Loads (Load String) takes a Json file and converts into python data structure (dict or list, depending on JSON)
-    print("loaded")
+    jData = json.loads(myResponse.content)
+	
+	print("The response contains {0} properties".format(len(jData)))
+	print("\n")
+	for key in jData:
+		print key + " : " + jData[key]
 	
 else:
   # If response code is not ok (200), print the resulting http error code with description
