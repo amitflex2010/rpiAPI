8cfe22416b6860d251243587baa1e041b22f9c63
Amit Tripathi <contact.tripathi@gmail.com>
2019-04-29 00:34:16 +0000

    update file

--- Neopixel/RestfulGetClient.py
+++ Neopixel/RestfulGetClient.py
@@ -43,25 +43,27 @@ def resetLeds(ring, color, wait_ms=10):
                 ring.setPixelColor(i, color)
                 ring.show()
 
-# It is a good practice not to hardcode the credentials. So ask the user to enter credentials at runtime
-myResponse = requests.get(url)
-ring = Adafruit_NeoPixel(LEDS , PIN , 800000 , 7 , False , BRIGHTNESS)
-ring.begin()
+# It is a good practice not to hardcode the credentials. So ask the user to enter credentials at runtim
+if __name__ == '__main__':
+
+    myResponse = requests.get(url)
+    ring = Adafruit_NeoPixel(LEDS , PIN , 800000 , 7 , False , BRIGHTNESS)
+    ring.begin()
 #print (myResponse.status_code)
 
 # For successful API call, response code will be 200 (OK)
-if(myResponse.ok):
+    if(myResponse.ok):
 
 	# Loading the response data into a dict variable
 	# json.loads takes in only binary or string variables so using content to fetch binary content
-	# Loads (Load String) takes a Json file and converts into python data structure (dict or list, depending on JSON)
-	jData = json.loads(myResponse.content)
+        # Loads (Load String) takes a Json file and converts into python data structure (dict or list, depending on JSON)
+	    jData = json.loads(myResponse.content)
 	
         
-    loopLed (ring, Color(KLEUR_G, 0, 0),100)
+            loopLed (ring, Color(KLEUR_G, 0, 0),100)
 	
 
-else:
+    else:
   # If response code is not ok (200), print the resulting http error code with description
-  	loopLed (ring, Color(0, KLEUR_R, 0),100)
-	myResponse.raise_for_status()
+    	    loopLed (ring, Color(0, KLEUR_R, 0),100)
+	    myResponse.raise_for_status()
