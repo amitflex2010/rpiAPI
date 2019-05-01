29c025a84f3d8812a0d940cd57647e8209153102
Amit Tripathi <contact.tripathi@gmail.com>
2019-05-01 23:46:19 +0000

    Final fix

--- Neopixel/RestfulGetClient.py
+++ Neopixel/RestfulGetClient.py
@@ -17,10 +17,10 @@ PIN         = 18     # GPIO 18 / PIN 12
 BRIGHTNESS  = 55     # min 0 / max 255
 
 KLEUR_R     = 255
-KLEUR_G     = 0
+KLEUR_G     = 255
 KLEUR_B     = 0
 
-LED_1_COUNT      = 30      # Number of LED pixels.
+LED_1_COUNT      = 7      # Number of LED pixels.
 LED_1_PIN        = 18      # GPIO pin connected to the pixels (must support PWM! GPIO 13 and 18 on RPi 3).
 LED_1_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_1_DMA        = 10      # DMA channel to use for generating signal (Between 1 and 14)
@@ -137,9 +137,9 @@ def initialSetup():
 ring = ring = Adafruit_NeoPixel(LED_1_COUNT, LED_1_PIN, LED_1_FREQ_HZ, LED_1_DMA, LED_1_INVERT, LED_1_BRIGHTNESS, LED_1_CHANNEL, LED_1_STRIP)
 ring.begin()
 initialSetup()
-#s = sched.scheduler(time.time, time.sleep)
-#s.enter(10, 1, pollEndPoint, (s,))
-#s.run()
+s = sched.scheduler(time.time, time.sleep)
+s.enter(10, 1, pollEndPoint, (s,))
+s.run()
 
 
 	
